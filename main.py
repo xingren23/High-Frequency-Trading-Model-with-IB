236679a183823d18fb975715788d262c46028c57
wang <wangzg@xueqiu.com>
2017-06-06 09:27:02 +0800

    port 4001

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from models.hft_model import HFTModel
 
 if __name__ == "__main__":
     model = HFTModel(host='localhost',
-                     port=7496,
+                     port=4001,
                      client_id=101,
-                     is_use_gateway=False,
+                     is_use_gateway=True,
                      evaluation_time_secs=20,
                      resample_interval_secs='30s')
     model.start(["JPM", "C"], 100)
